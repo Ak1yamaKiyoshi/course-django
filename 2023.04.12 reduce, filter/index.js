5048d1f0e5a9c262e262710369de275708ce6fd8
akiyama_kiyoshi <108092986+Ak1yamaKiyoshi@users.noreply.github.com>
2023-04-17 21:30:48 +0300

    Update index.js

--- 2023.04.12 reduce, filter/index.js
+++ 2023.04.12 reduce, filter/index.js
@@ -6,25 +6,32 @@ function sumOfEvenNumbersMultipliedByTwo(array) {
     array = array.map(n => n *= 2); /* multiply each by 2 */
     return array.reduce((acc, element) => {return acc + element }); 
 }
+console.log(sumOfEvenNumbersMultipliedByTwo([1, 2, 3, 4, 5, 6, 7, 8, 10])) // 60
+
 
 // Напишіть функцію, яка приймає два аргументи - ім'я та вік. 
 // Функція повинна повернути рядок у форматі 
 // "Привіт, мене звати <ім'я> та мені <вік> років.", використовуючи стрілочну функцію.
-// makes first letter of name uppercase too 
+// ! makes first letter of name uppercase too 
+// name.charAt(0).toUpperCase() + name.slice(1)
 let greeting = (name, age) => {return `Hello, my name is ${name.charAt(0).toUpperCase() + name.slice(1)}. I'm ${age}.`};
+console.log(greeting('someone', 13)); // Hello, my name is Someone. I'm 13.
+
 
 // Створіть функцію, яка приймає довільну кількість аргументів та виводить їх у консоль. 
 // Потім викличте цю функцію, передавши їй кілька аргументів за допомогою spread-оператора.
 function printAllParamteres() {
     for (let i = 0; i < arguments.length; i++)
-    console.log(arguments[i]);
-} // idk(...[1, 2, ('b'+'a'+ +'a'+'a').toLowerCase()] )
+    console.log(arguments[i]); 
+} 
+printAllParamteres(...[1, 2, ('b'+'a'+ +'a'+'a').toLowerCase()] ) // 1 2 'banana'
+
 
 // Створіть об'єкт Map, у якому ключами будуть імена країн, а значеннями - їхні столиці. 
 // Заповніть цей об'єкт кількома парами ключ-значення і виведіть їх у консоль.
-// let countries = new Map; 
-// countries.set('Ukraine', 'Kiyv');
-// countries.set('England', 'London');
-// countries.set('France', 'Paris');
-// for (let country of countries) 
-//     console.log(`${country[0]}: ${country[1]}`)
+let countries = new Map; 
+countries.set('Ukraine', 'Kiyv');
+countries.set('England', 'London');
+countries.set('France', 'Paris');
+for (let country of countries) 
+    console.log(`${country[0]}: ${country[1]}`) // 'Ukraine: Kiyv' 'England: London' 'France: Paris'
